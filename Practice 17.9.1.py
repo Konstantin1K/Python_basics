43e91f1e9d594862c8f49fa7535653b707939f0f
Konstantin1K <116068734+Konstantin1K@users.noreply.github.com>
2022-11-23 09:37:22 +0300

    Add files via upload

---
+++ Practice 17.9.1.py
@@ -0,0 +1,31 @@
+L = input("Введите последовательность чисел через пробел: ")
+L_mod = [int(a) for a in L.split()]
+
+if (' ' not in (L)):
+    raise ValueError("Введены некорректные данные! Введите последовательность чисел через пробел!")
+
+num = int(input("Введите любое число: "))
+if num % 1 != 0:
+    raise ValueError("Введены некорректные данные! Введите число!")
+
+for i in range(1, len(L_mod)):
+    x = L_mod[i]
+    idx = i
+    while idx > 0 and L_mod[idx-1] > x:
+        L_mod[idx] = L_mod[idx-1]
+        idx -= 1
+    L_mod[idx] = x
+print("Сортированный список:", L_mod)
+
+def binary_search(L_mod, num, left, right):
+    if left > right:
+        return False
+    middle = (right + left) // 2
+    if L_mod[middle] == num:
+        return middle
+    elif num < L_mod[middle]:
+        return binary_search(L_mod, num, left, middle - 1)
+    else:
+        return binary_search(L_mod, num, middle + 1, right)
+
+print(binary_search(L_mod, num, 0, len(L_mod)-1))
