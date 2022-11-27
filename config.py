d458967884b64d4415db895c44d767566c530288
Konstantin1K <116068734+Konstantin1K@users.noreply.github.com>
2022-11-27 11:02:41 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+TOKEN = '5901524248:AAGRagg4TYYg-MAL9_HpMJpSjfOwzMPUsKc'
+keys = {'Доллар': 'USD', 'Евро': 'EUR', 'Рубль': 'RUB'}
+api_key = 'b413ffe07bee1ef3cc9533796c1433ca29a62f1d3e8e0b37bc61121f957b662b'
